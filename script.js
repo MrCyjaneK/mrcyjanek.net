6aa0107ff9de45fd82dac42eea69eb3ea7a3a88d
mrcyjanek <mrcyjanek@gmail.com>
2019-07-18 08:59:09 +0200

    more cool stuff addded

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log(`Okey, I don't know what are you doing in here. But github.com/mrcyjanek/mrcyjanek.net is probably what are you looking for. I allow you to do whatever you want here, or with this code, no need to mention me.`);
 function cool_redirect(url) {
 	itemloop = []
-	array = [1,2,3,4,5,6,7,8,9,10,11];
+	array = [1,2,3,4,5,6,7,8,9,10,11,12];
 	array.forEach( item => {
 	item=document.getElementById(item)
 	i=0
@@ -9,15 +9,28 @@ function cool_redirect(url) {
 		item.innerText = item.innerText.substr(0,item.innerText.length-1)
 		if (item.innerText=="") {
 			i++;
-			console.log(i)
 			clearInterval(itemloop[item.id])
 	    }
 	},Number(1200/item.innerText.length).toFixed(0))
 	setInterval(() => {
-		if (i == 11) {
+		if (i == 12) {
 			setTimeout(() => {window.location.href=url;},200);
 			document.body.innerHTML = '<link rel="stylesheet" href="style.css">'
 	    }
 	},42)
 	})
+}
+function cool_show(what,where,href = '#') {
+	item = document.getElementById(where)
+	begin = item.innerText
+	target = what
+	i = 0
+	y = setInterval(() => {
+		item.innerText = target.substr(0,i-1)+begin.substr(i)
+		i++;
+		if (item.innerText == target) {
+			clearInterval(y)
+			item.href = href
+		}
+	},Number(2048/target.length).toFixed(0))
 }
